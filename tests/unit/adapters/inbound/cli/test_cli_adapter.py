429d2c1f8bf4bcef6be2ce2a0c1bef16c0e85c85
Augusto <you@example.com>
2025-10-01 13:45:24 -0300

    Testes

--- tests/unit/adapters/inbound/cli/test_cli_adapter.py
+++ tests/unit/adapters/inbound/cli/test_cli_adapter.py
@@ -1,6 +1,6 @@
 from typer.testing import CliRunner
 
-from src.apiops_orchestrator.adapters.inbound.cli.cli_adapter import app
+from apiops_orchestrator.adapters.inbound.cli.cli_adapter import app
 
 runner = CliRunner()
 
@@ -17,10 +17,15 @@ def test_sync_openapi_help():
 
 def test_sync_openapi_missing_env_fails():
     """Test that the command fails if the required --env option is missing."""
-    result = runner.invoke(app, ["sync-openapi"])
+    result = runner.invoke(app, ["sync-openapi", "--repo", "/test"])
     assert result.exit_code != 0
     assert "Missing option '--env' / '-e'." in result.output
 
+def test_sync_openapi_missing_repo_fails():
+    """Test that the command fails if the required --repo option is missing."""
+    result = runner.invoke(app, ["sync-openapi", "--env", "test"])
+    assert result.exit_code != 0
+    assert " Missing option '--repo' / '-r'" in result.output
 
 # TODO Implementar caso de sucesso default.
 def test_sync_openapi_defaults():
