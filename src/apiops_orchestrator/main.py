b60925c26121b46e0e86bc76717dceb234a6e088
Augusto <you@example.com>
2025-11-13 12:24:21 -0300

    refactor: repo_cep_path --> repo_path

--- src/apiops_orchestrator/main.py
+++ src/apiops_orchestrator/main.py
@@ -94,7 +94,7 @@ def generate_api_json(
 def main() -> None:
     """Main orchestration function."""
     settings = Settings()
-    repo_cep_path = (
+    repo_path = (
         settings.PROJECT_ROOT / settings.API_REPO_FOLDER
     )  # This Path is the default for the pipeline. If you're running locally, change this Path to your local API Repository.
 
@@ -111,15 +111,15 @@ def main() -> None:
     }
 
     try:
-        validate_repository_structure(repo_validator, repo_cep_path, settings)
+        validate_repository_structure(repo_validator, repo_path, settings)
 
-        import_repository_files(file_importer_service, repo_cep_path, settings)
+        import_repository_files(file_importer_service, repo_path, settings)
 
         validate_repository_schemas(
-            schema_validator, file_importer_service, repo_cep_path, schema_mapping
+            schema_validator, file_importer_service, repo_path, schema_mapping
         )
 
-        final_json = generate_api_json(file_importer_service, repo_cep_path, settings)
+        final_json = generate_api_json(file_importer_service, repo_path, settings)
 
         _print_step("Final Result: API JSON")
         print(json.dumps(final_json.model_dump(), indent=2, ensure_ascii=False))
