30dae67a6478a90f2954ba8f45b39c795ba3657e
Augusto <you@example.com>
2025-11-13 17:36:45 -0300

    feat: removing utf-8

--- src/apiops_orchestrator/adapters/inbound/files_importer/local_file_importer_adapter.py
+++ src/apiops_orchestrator/adapters/inbound/files_importer/local_file_importer_adapter.py
@@ -37,7 +37,7 @@ class LocalFileLoaderAdapter(PathLoaderPort):
         ]
 
     def _load_single_file(self, file_path: Path) -> Any:
-        self._ensure_utf8_encoding(file_path)
+        # self._ensure_utf8_encoding(file_path)
 
         suffix = file_path.suffix.lower()
         loader = self._loader_strategies.get(suffix)
@@ -51,6 +51,8 @@ class LocalFileLoaderAdapter(PathLoaderPort):
             )
         return loader(file_path)
 
+
+""" 
     @staticmethod
     def _ensure_utf8_encoding(file_path: Path) -> None:
         try:
@@ -60,3 +62,4 @@ class LocalFileLoaderAdapter(PathLoaderPort):
             raise InvalidFileEncodingError(
                 f"{file_path} is not valid UTF-8 encoded: {e}"
             ) from e
+"""
