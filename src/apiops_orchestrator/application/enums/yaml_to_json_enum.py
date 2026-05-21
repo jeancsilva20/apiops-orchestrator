1de0fc2145d93ea103c9a750f71737431f7ade9c
Augusto <you@example.com>
2026-05-21 14:20:13 -0300

    fix: revision info missing

--- src/apiops_orchestrator/application/enums/yaml_to_json_enum.py
+++ src/apiops_orchestrator/application/enums/yaml_to_json_enum.py
@@ -1,16 +1,10 @@
 from enum import Enum
 
 
-class JsonKind(Enum):
+class YamlKind(Enum):
     API_BASIC_INFO = "ApiBasicInfo"
     INTERCEPTORS = "Interceptors"
     API_OPERATIONS = "ApiOperations"
     RESOURCES_LIST = "ResourcesList"
     ENVIRONMENT = "Environment"
     REVISION_INFO = "RevisionInfo"
-
-
-class JsonKindFileName(Enum):
-    API_BASIC_INFO = "api-basic-info.yaml"
-    INTERCEPTORS = "default-interceptors.yaml"
-    RESOURCES = "resources.yaml"
