a189354eae6d3d55fb7fde6719f9fb9962329350
luiza silva <45200096+luizanascimentoesilva@users.noreply.github.com>
2026-05-27 09:24:18 -0300

    Ajuste da localização da pasta do repositório

--- src/apiops_orchestrator/application/services/versioner_service.py
+++ src/apiops_orchestrator/application/services/versioner_service.py
@@ -29,7 +29,8 @@ class VersionerService:
         self.repo = repo_adapter
         self.manager = manager_adapter
         self.settings = settings
-        self.repo_path = settings.PROJECT_ROOT / settings.API_REPO_FOLDER
+        #self.repo_path = settings.PROJECT_ROOT / settings.API_REPO_FOLDER
+        self.repo_path = settings.PROJECT_ROOT
         self.revisions_folder = self.repo_path / "src" / "apis" / "revisions"
         self.file_generator = RevisionFileGenerator(repo_adapter)
 
